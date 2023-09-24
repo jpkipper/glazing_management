7958fc1e68c24db143ed8b0a2e25437eaf473d6d
João P. Kipper <jpkipper@hotmail.com>
2023-09-24 19:42:03 -0300

    atualizando projeto

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,18 @@
-declare global {
-  namespace NodeJS {
-    interface Global {
-      prisma?: import(".prisma/client").PrismaClient;
-    }
+import { PrismaClient } from '@prisma/client';
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  //@ts-ignore
+  if (!global.prisma) {
+    //@ts-ignore
+    global.prisma = new PrismaClient();
   }
+
+  //@ts-ignore
+  prisma = global.prisma;
 }
-
-let prisma;
-
-if (typeof global.prisma === "undefined") {
-  global.prisma = new PrismaClient();
-}
-
-prisma = global.prisma;
 
 export default prisma;
