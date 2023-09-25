1b7e18fcd79153215a805d5222fc7d73e9b1b038
João P. Kipper <jpkipper@hotmail.com>
2023-09-24 23:31:27 -0300

    Atualizando tabelas

---
+++ prisma/migrations/20230925023023_prima_migration/migration.sql
@@ -0,0 +1,319 @@
+/*
+  Warnings:
+
+  - You are about to drop the `_GroupToPermission` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `_GroupToUser` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "_GroupToPermission" DROP CONSTRAINT "_GroupToPermission_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_GroupToPermission" DROP CONSTRAINT "_GroupToPermission_B_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_GroupToUser" DROP CONSTRAINT "_GroupToUser_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_GroupToUser" DROP CONSTRAINT "_GroupToUser_B_fkey";
+
+-- DropTable
+DROP TABLE "_GroupToPermission";
+
+-- DropTable
+DROP TABLE "_GroupToUser";
+
+-- CreateTable
+CREATE TABLE "itm_types" (
+    "id" SERIAL NOT NULL,
+    "state" INTEGER NOT NULL,
+    "ref_category" INTEGER NOT NULL,
+    "ref_owner" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "info" TEXT NOT NULL,
+    "ref_serial" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "itm_types_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "itm_fields" (
+    "id" SERIAL NOT NULL,
+    "ref_type" INTEGER NOT NULL,
+    "label" TEXT NOT NULL,
+    "position" INTEGER NOT NULL,
+    "data_type" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "itm_fields_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Item" (
+    "id" SERIAL NOT NULL,
+    "ref_owner" INTEGER NOT NULL,
+    "ref_type" INTEGER NOT NULL,
+    "state" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "serial" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "value" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Item_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "itm_values" (
+    "id" SERIAL NOT NULL,
+    "ref_item" INTEGER NOT NULL,
+    "ref_field" INTEGER NOT NULL,
+    "content" TEXT NOT NULL,
+    "type" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "itm_values_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "itm_stock" (
+    "id" SERIAL NOT NULL,
+    "ref_item" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "itm_stock_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "prod_types" (
+    "id" SERIAL NOT NULL,
+    "state" INTEGER NOT NULL,
+    "ref_category" INTEGER NOT NULL,
+    "ref_owner" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "info" TEXT NOT NULL,
+    "ref_serial" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "prod_types_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "prod_fields" (
+    "id" SERIAL NOT NULL,
+    "ref_type" TEXT NOT NULL,
+    "label" TEXT NOT NULL,
+    "postion" INTEGER NOT NULL,
+    "data_type" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "prod_fields_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "ref_owner" INTEGER NOT NULL,
+    "ref_type" INTEGER NOT NULL,
+    "state" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "serial" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "value" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "prod_values" (
+    "id" SERIAL NOT NULL,
+    "ref_product" INTEGER NOT NULL,
+    "ref_field" INTEGER NOT NULL,
+    "content" TEXT NOT NULL,
+    "type" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "prod_values_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "prod_items_to_prod" (
+    "id" SERIAL NOT NULL,
+    "ref_item" INTEGER NOT NULL,
+    "ref_product" INTEGER NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "prod_items_to_prod_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "base_categories" (
+    "id" SERIAL NOT NULL,
+    "ref_owner" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "state" INTEGER NOT NULL,
+    "ref_module" INTEGER NOT NULL,
+    "ref_serial" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "base_categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "base_serials" (
+    "id" SERIAL NOT NULL,
+    "start_value" INTEGER NOT NULL,
+    "current_value" INTEGER NOT NULL,
+    "pattern" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "ref_module" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "base_serials_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "base_users_groups" (
+    "id" SERIAL NOT NULL,
+    "ref_user" INTEGER NOT NULL,
+    "ref_group" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "base_users_groups_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "base_group_permissions" (
+    "id" SERIAL NOT NULL,
+    "ref_group" INTEGER NOT NULL,
+    "ref_permission" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "base_group_permissions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Provider" (
+    "id" SERIAL NOT NULL,
+    "state" INTEGER NOT NULL,
+    "ref_owner" INTEGER NOT NULL,
+    "public_name" TEXT NOT NULL,
+    "info" TEXT NOT NULL,
+    "uf" TEXT NOT NULL,
+    "cep" TEXT NOT NULL,
+    "legal_name" TEXT NOT NULL,
+    "cnpj" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "count" TEXT NOT NULL,
+    "company_owner" TEXT NOT NULL,
+    "company_owner_cpf" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Provider_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Purchase" (
+    "id" SERIAL NOT NULL,
+    "ref_item" INTEGER NOT NULL,
+    "value" DOUBLE PRECISION NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Purchase_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sup_purchasing_suppliers" (
+    "id" SERIAL NOT NULL,
+    "ref_provider" INTEGER NOT NULL,
+    "ref_purchase" INTEGER NOT NULL,
+    "total" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "sup_purchasing_suppliers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sup_provider_items" (
+    "id" SERIAL NOT NULL,
+    "ref_item" INTEGER NOT NULL,
+    "ref_provider" INTEGER NOT NULL,
+    "code" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "sup_provider_items_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Custumer" (
+    "id" SERIAL NOT NULL,
+    "identity" INTEGER NOT NULL,
+    "ref_owner" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "info" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "uf" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "cep" TEXT NOT NULL,
+    "county" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Custumer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Request" (
+    "id" SERIAL NOT NULL,
+    "ref_custumer" INTEGER NOT NULL,
+    "ref_owner" INTEGER NOT NULL,
+    "value" DOUBLE PRECISION NOT NULL,
+    "dt_request" TIMESTAMP(3) NOT NULL,
+    "dt_delivery" TIMESTAMP(3) NOT NULL,
+    "delivered" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Request_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "crm_products_to_request" (
+    "id" SERIAL NOT NULL,
+    "ref_request" INTEGER NOT NULL,
+    "ref_product" INTEGER NOT NULL,
+    "value" DOUBLE PRECISION NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "crm_products_to_request_pkey" PRIMARY KEY ("id")
+);
