b4503a1d08820a70e05a0ec6049e7f86784d093c
João P. Kipper <jpkipper@hotmail.com>
2023-09-24 21:15:27 -0300

    Atualizando pages

--- src/navigation/vertical/index.ts
+++ src/navigation/vertical/index.ts
@@ -1,6 +1,11 @@
 // ** Icon imports
 import AccountMultiplePlus from 'mdi-material-ui/AccountMultiplePlus'
+import Finance from 'mdi-material-ui/Finance'
+import CogOutline from 'mdi-material-ui/CogOutline'
 import AccountGroup from 'mdi-material-ui/AccountGroup'
+import ArrangeSendToBack from 'mdi-material-ui/ArrangeSendToBack'
+import AccountBoxMultiple from 'mdi-material-ui/AccountBoxMultiple'
+import Cart from 'mdi-material-ui/Cart'
 import HomeOutline from 'mdi-material-ui/HomeOutline'
 
 // ** Type import
@@ -14,24 +19,66 @@ const navigation = (): VerticalNavItemsType => {
       path: '/'
     },
     {
-      sectionTitle: 'Cadastros'
+      sectionTitle: 'Pages'
     },
     {
-      title: 'Usuários',
+      title: 'Clientes',
       icon: AccountMultiplePlus,
-      path: '/users',
+      path: '/customers',
       openInNewTab: false
     },
     {
-      title: 'Grupos',
+      title: 'Compras Fornecedores',
+      icon: AccountMultiplePlus,
+      path: '/customers',
+      openInNewTab: false
+    },
+    {
+      title: 'Fornecedores',
+      icon: AccountBoxMultiple,
+      path: '/permissions',
+      openInNewTab: false
+    },
+    {
+      title: 'Grupos-exemplo',
       icon: AccountGroup,
       path: '/groups',
       openInNewTab: false
     },
     {
-      title: 'Permissões',
+      title: 'Itens',
+      icon: ArrangeSendToBack,
+      path: '/items',
+      openInNewTab: false
+    },
+    {
+      title: 'Permissões-exemplo',
       icon: AccountMultiplePlus,
       path: '/permissions',
+      openInNewTab: false
+    },
+    {
+      title: 'Produtos',
+      icon: CogOutline,
+      path: '/products',
+      openInNewTab: false
+    },
+    {
+      title: 'Pedidos',
+      icon: Cart,
+      path: '/products',
+      openInNewTab: false
+    },
+    {
+      title: 'Relatórios',
+      icon: Finance,
+      path: '/products',
+      openInNewTab: false
+    },
+    {
+      title: 'Usuários',
+      icon: AccountMultiplePlus,
+      path: '/users',
       openInNewTab: false
     },
   ]
